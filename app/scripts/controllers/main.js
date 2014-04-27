1bef208e1b8b1465965d0a0d4283ff671368f5c6
Sergey Sidorchick <whitekrokodile@gmail.com>
2014-04-27 12:33:43 +0300

    jQuery ajax replaced with $http service.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,16 @@
 'use strict';
 
 angular.module('cyanogenmodDistributionApp')
-  .controller('MainCtrl', function ($scope) {
-    $.getJSON('scripts/data-04-07-2014.json', function(data) {
-      var version = data.result.version;
-      var builds =
-        _.chain(version)
-          .map(function(v) { return { name: v[1], downloads: v[0] }; })
-          .filter(function(build) { return build.downloads > 10000; })
-          .value();
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('scripts/data-04-07-2014.json')
+      .success(function(data) {
+        var version = data.result.version;
+        var builds =
+          _.chain(version)
+            .map(function(v) { return { name: v[1], downloads: v[0] }; })
+            .filter(function(build) { return build.downloads > 10000; })
+            .value();
 
-      $scope.data = builds;
-    });
+        $scope.data = builds;
+      });
   });
